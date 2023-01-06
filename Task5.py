bfb1cd1c0f258b61d92ad3cc27b150e2df6adbf1
TataBrn <96158820+TataBrn@users.noreply.github.com>
2023-01-06 06:33:00 +0300

    Task5

---
+++ Task5.py
@@ -0,0 +1,11 @@
+"""
+Есть список numbers = [0, 99, 100, 53, 44, 23, 4, 8, 16, 15, 77, 51]
+Используя модуль random, вывести случайный элемент этого списка.
+"""
+
+from random import randint
+
+numbers = [0, 99, 100, 53, 44, 23, 4, 8, 16, 15, 77, 51]
+
+rnd_index = randint(0, len(numbers)-1)
+print(numbers[rnd_index])
